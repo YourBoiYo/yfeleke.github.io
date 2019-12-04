46771378b04e4255de43913d382ead044696b24b
Yohan-git <38788515+YourBoiYo@users.noreply.github.com>
2019-12-03 21:47:37 -0500

    Add files via upload

---
+++ results.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <<link rel="stylesheet" href="layout.css">
+  <title>Admin Dashboard</title>
+</head>
+
+<?php
+  $is147 = 24;
+  $is247 = 30;
+  $is300 = 42;
+  $is310 = 52;
+  $is410 = 30;
+  $is420 = 16;
+  $is425 = 20;
+  $is436 = 25;
+  $is450 = 28;
+  $is451 = 27;
+?>
+<body>
+    <div class="dashboard">
+      <table>
+        <tr>
+          <th>Class Name</th>
+          <th>Votes</th>
+        </tr>
+        <tr>
+          <td>IS147</td>
+          <td><?php echo $is147; ?></td>
+        </tr>
+        <tr>
+          <td>IS247</td>
+          <td><?php echo $is247; ?></td>
+        </tr>
+        <tr>
+          <td>IS300</td>
+          <td><?php echo $is300; ?></td>
+        </tr>
+        <tr>
+          <td>IS310</td>
+          <td><?php echo $is310; ?></td>
+        </tr>
+        <tr>
+          <td>IS410</td>
+          <td><?php echo $is410; ?></td>
+        </tr>
+        <tr>
+          <td>IS420</td>
+          <td><?php echo $is420; ?></td>
+        </tr>
+        <tr>
+          <td>IS425</td>
+          <td><?php echo $is425; ?></td>
+        </tr>
+        <tr>
+          <td>IS436</td>
+          <td><?php echo $is436; ?></td>
+        </tr>
+        <tr>
+          <td>IS450</td>
+          <td><?php echo $is450; ?></td>
+        </tr>
+        <tr>
+          <td>IS451</td>
+          <td><?php echo $is451; ?></td>
+        </tr>
+      </table>
+
+    </div>
+</body>
